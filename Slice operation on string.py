3696f1f89562f825a0b90a8e9118108282016e14
Kritagyabhagat <117289078+Kritagyabhagat@users.noreply.github.com>
2024-02-20 00:26:23 +0530

    Update

---
+++ Slice operation on string.py
@@ -0,0 +1,10 @@
+# String Operations
+str = 'Python is Easy !!!'
+print(str)
+print(str[0])
+print(str[3:9])
+print(str[4:])
+print(str[-1])
+print(str[:5])
+print(str * 3)
+print(str + "ISN'T IT?")
---
+++ Sum of first N natural number.py
@@ -0,0 +1,7 @@
+N = 20
+I = 1
+sum = 0
+while I <= N :
+  sum = sum + I
+  I = I + 1
+print(sum)
